aff3bd76ad34575ec589fb1d97f3413e10568ae8
Tanuj Thakkar <tanuj@phantomauto.com>
2022-10-12 15:27:57 -0400

    Updated main.cpp, AnalogSensor.cpp
    1. Initialized terminator in main.cpp
    2. Removed use of points in AnalogSensor.cpp

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -13,9 +13,9 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    std::vector<int> readings(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate( readings.begin(), readings.end(), 0.0 ) / readings.size();
     return result;
 }
 
